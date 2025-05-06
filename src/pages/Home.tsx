cedfd4df3d67798d199c3a74aa92b78ff7ef2b36
Kauan-dot <kauanaffonso115@gmail.com>
2025-05-06 15:21:58 -0300

    Feat: Avatars List Components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,33 @@
-import { CardComponent, Header } from '@/components'
+import { AvatarsList, CardComponent, Header } from '@/components'
 import { Container } from '@mui/system'
+import { currencyConverter } from '@/utils'
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(1234.34),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(4647.55),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 3',
+      subtitle: currencyConverter(8847.78),
+    },
+  ]
   return (
     <>
       <Header />
       <Container maxWidth="lg">
         <CardComponent>CARD</CardComponent>
+        <CardComponent>
+          <AvatarsList listData={mockListData} />
+        </CardComponent>
       </Container>
     </>
   )
