fd5e1146d1da07102f09449463d0dbe138dc4f86
Kauan-dot <kauanaffonso115@gmail.com>
2025-06-07 15:28:40 -0300

    Fix: test

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,11 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
 import { TextEncoder, TextDecoder } from 'util'
 
 if (typeof global.TextEncoder === 'undefined') {
-  global.TextEncoder = TextEncoder
+  global.TextEncoder = TextEncoder as any
 }
 
 if (typeof global.TextDecoder === 'undefined') {
-  global.TextDecoder = TextDecoder
+  global.TextDecoder = TextDecoder as any
 }
